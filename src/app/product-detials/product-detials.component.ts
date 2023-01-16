342e48ee6e2802b7746300b25e386524f78aa316
RahulNetsmartz1933 <122775023+RahulNetsmartz1933@users.noreply.github.com>
2023-01-16 16:16:12 +0530

    3 files modified, 8 added

---
+++ src/app/product-detials/product-detials.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detials',
+  templateUrl: './product-detials.component.html',
+  styleUrls: ['./product-detials.component.css']
+})
+export class ProductDetialsComponent {
+
+}
